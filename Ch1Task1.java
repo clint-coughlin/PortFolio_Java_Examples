7770a2d3e57ab82bb5cd4a3263e20fad6219c6dc
clint-coughlin <clint_coughlin@insideranken.org>
2017-03-10 13:30:16 -0600

    Chapter 1 Labs

---
+++ Ch1Task1.java
@@ -0,0 +1,16 @@
+
+/**
+ * @author Clint Coughlin
+ * My First program
+ */
+public class Ch1Task1 {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World! This is my first Program!");
+		// exit the system process
+		System.exit(0);
+	}
+
+}
